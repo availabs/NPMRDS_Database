545a75aa299ec8b3080d84105dd8ca37f93f3e4f
Paul Tomchik <pauljtomchik@live.com>
2018-03-08 17:18:30 -0500

    More for phed_table_simplification task.

---
+++ bin/phed_table_simplification/backup_phed_tables.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#  npmrds_local=# drop table tmc_attributes ;
+#  ERROR:  cannot drop table tmc_attributes because other objects depend on it
+#  DETAIL:  materialized view geography_level_to_states depends on table tmc_attributes
+#  materialized view ny.pm_bottlenecks_summary depends on table tmc_attributes
+#  materialized view geography_level_attributes_view depends on table tmc_attributes
+#  table nj.tmc_attributes depends on table tmc_attributes
+#  table ny.tmc_attributes depends on table tmc_attributes
+#  HINT:  Use DROP ... CASCADE to drop the dependent objects too.
+
+set -e
+
+mkdir -p sql
+
+# source ../../config/postgres.env.local
+source ../../config/postgres.env.ares
+
+export PGDATABASE
+export PGUSER
+export PGPASSWORD
+export PGHOST
+export PGPORT
+
+pg_dump --clean --no-owner \
+  --table='phed' \
+  --table='nj.phed*' \
+  --table='ny.phed*' > sql/phed.backup.sql
---
+++ bin/phed_table_simplification/recalculatePHED.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+cd ../../
+
+# source ./config/postgres.env.local
+source ./config/postgres.env.ares
+
+export PGDATABASE
+export PGUSER
+export PGPASSWORD
+export PGHOST
+export PGPORT
+
+SQL="
+  SELECT *
+    FROM (
+      SELECT
+          'STATE=' || schemaname ||
+          ' YEAR=' || substring(tablename from 9 for 4) ||
+          ' MONTH=' || substring(tablename from 14 for 2)
+        FROM pg_tables
+        WHERE (tablename LIKE '%npmrds_y%')
+      UNION
+      SELECT DISTINCT
+          'STATE=' || schemaname ||
+          ' YEAR=' || substring(tablename from 9 for 4) ||
+          ' MONTH=00'
+        FROM pg_tables
+        WHERE (tablename LIKE '%npmrds_y%')
+    ) AS t(c)
+    ORDER BY c
+;"
+
+OUTPUT=$(psql --tuples-only -c "${SQL}")
+
+echo "${OUTPUT}" |\
+while read -r line ; do
+
+  eval "${line}"
+
+  export STATE
+  export YEAR
+  export MONTH
+
+  time make db/load-state-phed-yrmo-table
+done
+
