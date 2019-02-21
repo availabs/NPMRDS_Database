e8e0ef12f4fce50ccde14afef20a79a6fac7df8d
avail <avail@avail-lor.ba.albany.edu>
2019-02-20 19:56:59 -0500

    Found some of dvad's old code and added to tasks.

---
+++ tasks/dvads-road-network-building-code/create_tmc_adjs.sql
@@ -0,0 +1,25 @@
+﻿--DROP TABLE IF EXISTS tmc_adjs_dest;
+CREATE VIEW tmc_120p11204_child as (
+
+WITH baseverts AS (
+WITH ibase AS(
+SELECT distinct shp.source, shp.target
+	FROM public."npmrds_shapefile_2016Q2" AS shp
+	JOIN public."npmrds_tmc_lut_2016Q2" AS lut USING (link_id)
+	WHERE lut.tmc='120P11204'
+)
+ SELECT ibase.source from ibase
+ UNION
+ SELECT ibase.target from ibase
+)
+
+SELECT '120P11204', term.tmc as child--, CAST( ST_MULTI(ST_UNION(array_agg(shp.wkb_geometry))) as geometry(MultiLineString,4326) )
+  FROM public.tmc_terminals as terms
+  LEFT JOIN public.tmc_origins as origin USING (tmc)
+  WHERE origin is NULL and terms.id in baseverts
+  
+
+  
+  WHERE term.tmc <> lut.tmc and term.id = origin.origin
+  GROUP BY lut.tmc, term.tmc
+);
---
+++ tasks/dvads-road-network-building-code/create_tmc_terminals.sql
@@ -0,0 +1,8 @@
+﻿-- Query for tmc endpoints
+DROP TABLE IF EXISTS tmc_terminals;
+CREATE TABLE tmc_terminals as (
+Select lut.tmc,vert.id,count(lut.link_id) as cnt from public."npmrds_shapefile_2016Q2" as shp 
+JOIN public."npmrds_tmc_lut_2016Q2" as lut USING(link_id)
+JOIN public."npmrds_shapefile_2016Q2_vertices_pgr" as vert on shp.source=vert.id OR shp.target=vert.id
+GROUP BY vert.id, lut.tmc HAVING count(lut.link_id) = 1
+)
