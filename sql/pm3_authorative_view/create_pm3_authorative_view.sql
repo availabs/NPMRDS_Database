ac7d5ad8016de4f110659e14d314054d595ab56f
Paul Tomchik <pauljtomchik@gmail.com>
2019-05-01 21:35:16 -0400

    Added "state" and "is_canonical" columns to the pm3_authorative_view

--- sql/pm3_authorative_view/create_pm3_authorative_view.sql
+++ sql/pm3_authorative_view/create_pm3_authorative_view.sql
@@ -1,18 +1,22 @@
 CREATE VIEW public.pm3_authorative_view
 AS 
   SELECT
-      (metadata->>'measure') AS measure,
-      (metadata->>'year')::SMALLINT AS year,
-      metadata,
+      (a.metadata->>'state') AS state,
+      (b.metadata->>'measure') AS measure,
+      (b.metadata->>'year')::SMALLINT AS year,
+      (b.metadata->>'isCanonical')::BOOLEAN AS is_canonical,
+      b.metadata,
       tmc,
       attribute,
       value
-    FROM pm3_measure_calculator_metadata AS a
-      INNER JOIN pm3_eav_append_only AS b
-      ON (a.id = b.pm3meacalc_id)
+    FROM pm3_calculator_metadata AS a
+      INNER JOIN pm3_measure_calculator_metadata AS b
+        ON (a.id = b.pm3calc_id)
+      INNER JOIN pm3_eav_append_only AS c
+        ON (b.id = c.pm3meacalc_id)
     WHERE (
-      (a.authorative_start IS NOT NULL)
+      (b.authorative_start IS NOT NULL)
       AND
-      (a.authorative_end IS NULL)
+      (b.authorative_end IS NULL)
     )
 ;
