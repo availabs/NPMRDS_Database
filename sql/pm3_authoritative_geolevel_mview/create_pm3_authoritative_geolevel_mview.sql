3253d5e588071e8ed7fe0c7a73b3973c04e90862
Paul Tomchik <pauljtomchik@gmail.com>
2019-09-10 20:33:48 -0400

    Added comment

--- sql/pm3_authoritative_geolevel_mview/create_pm3_authoritative_geolevel_mview.sql
+++ sql/pm3_authoritative_geolevel_mview/create_pm3_authoritative_geolevel_mview.sql
@@ -511,6 +511,7 @@ EXECUTE 'CREATE MATERIALIZED VIEW pm3_authoritative_geolevel_mview AS
 
   UNION ALL
 
+  -- Because of GROUP BY, each individual state's portion of the UA
   SELECT
     ''UA'' AS geolevel,
     ua_code AS geocode,
