a2b24a480ef6e6b9a5bbfba10cddbd56175897a6
Paul Tomchik <pauljtomchik@gmail.com>
2020-04-10 23:05:46 -0400

    GeoLevel PM3 calculations calculate and load run script target.

--- sql/pm3_geolevel_calculation_versions/create_pm3_versioned_geolevel_measures_table.sql
+++ sql/pm3_geolevel_calculation_versions/create_pm3_versioned_geolevel_measures_table.sql
@@ -20,64 +20,18 @@ CLUSTER pm3.pm3_geolevel_calculation_versions
 CREATE OR REPLACE VIEW pm3.pm3_geolevel_calculation_versions_view
   AS
     SELECT
-        sub_pcvv.measure_class,
-        sub_pcvv.year,
-        sub_pcvv.major_version,
-        sub_pcvv.minor_version,
-        sub_pcvv.fix_version,
-        sub_pcvv.prerelease_label,
-        sub_pcvv.pm3calc_ids,
-        sub_pcvv.changelog,
-        sub_pcvv.is_authoritative,
-        sub_pcvv.version_id,
-        pgcv.states,
-        pgcv.state_codes,
-        pgcv.lottr_interstate,
-        pgcv.lottr_noninterstate,
-        pgcv.tttr_interstate,
-        pgcv.phed,
-        jsonb_object_agg(
-          (sub_pcvv.measure_metadata_kv).key,
-          (sub_pcvv.measure_metadata_kv).value
-        ) AS measure_metadata
-      FROM (
-        SELECT
-            measure_class,
-            year,
-            major_version,
-            minor_version,
-            fix_version,
-            prerelease_label,
-            pm3calc_ids,
-            changelog,
-            is_authoritative,
-            version_id,
-            jsonb_each(measure_metadata) AS measure_metadata_kv
-          FROM pm3.pm3_calculation_versions_view AS pcvv
-      ) AS sub_pcvv INNER JOIN pm3.pm3_geolevel_calculation_versions AS pgcv
-        ON (
-          ( sub_pcvv.version_id = pgcv.version_id )
-          AND
-          ( (sub_pcvv.measure_metadata_kv).key = ANY(pgcv.states) )
-        )
-      GROUP BY (
-        sub_pcvv.measure_class,
-        sub_pcvv.year,
-        sub_pcvv.major_version,
-        sub_pcvv.minor_version,
-        sub_pcvv.fix_version,
-        sub_pcvv.prerelease_label,
-        sub_pcvv.pm3calc_ids,
-        sub_pcvv.changelog,
-        sub_pcvv.is_authoritative,
-        sub_pcvv.version_id,
-        pgcv.states,
-        pgcv.state_codes,
-        pgcv.lottr_interstate,
-        pgcv.lottr_noninterstate,
-        pgcv.tttr_interstate,
-        pgcv.phed
-      )
+        measure_class,
+        year,
+        major_version,
+        minor_version,
+        fix_version,
+        prerelease_label,
+        pm3calc_ids,
+        changelog,
+        is_authoritative,
+        pgcv.*
+      FROM pm3.pm3_calculation_versions_view AS pcvv
+        INNER JOIN pm3.pm3_geolevel_calculation_versions AS pgcv USING (version_id)
 ;
 
 COMMIT
