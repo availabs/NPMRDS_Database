ee1b3dc0cbc30fddb9e811ddcd95514bffc8ec70
Paul Tomchik <pauljtomchik@gmail.com>
2020-06-10 17:19:30 -0400

    avg_speedlimits loader and SQL DDL refinements.

--- sql/avg_speedlimits/create_state_avg_speedlimits.sql
+++ sql/avg_speedlimits/create_state_avg_speedlimits.sql
@@ -1,6 +1,8 @@
-CREATE TABLE :"STATE".avg_speedlimits (
-  LIKE public.avg_speedlimits INCLUDING ALL
-);
+CREATE TABLE IF NOT EXISTS :"STATE".avg_speedlimits (
+  state          CHAR(2)  DEFAULT :'STATE' CHECK(state = :'STATE'),
+  tmc            VARCHAR  PRIMARY KEY,
+  avg_speedlimit REAL
+) INHERITS( public.avg_speedlimits ) ;
 
-ALTER TABLE :"STATE".avg_speedlimits
-  ALTER COLUMN state SET DEFAULT :'STATE';
+CLUSTER :"STATE".avg_speedlimits 
+  USING avg_speedlimits_pkey ;
--- sql/avg_speedlimits/finish_state_avg_speedlimits.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE :"STATE".avg_speedlimits
-  ALTER COLUMN state SET DEFAULT :'STATE',
-  ADD CONSTRAINT avg_speedlimits_pkey PRIMARY KEY (tmc),
-  ADD CONSTRAINT avg_speedlimits_state_check CHECK(state = :'STATE'),
-  INHERIT public.avg_speedlimits;
-
-CLUSTER :"STATE".avg_speedlimits 
-  USING avg_speedlimits_pkey;
