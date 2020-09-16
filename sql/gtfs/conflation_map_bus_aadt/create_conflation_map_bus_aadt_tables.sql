da668f4b3d9bf91431f90d656e79eea53fb143df
Paul Tomchik <pauljtomchik@gmail.com>
2020-09-16 06:30:20 -0400

    gtfs-conflation-aadt

--- sql/gtfs/conflation_map_bus_aadt/create_conflation_map_bus_aadt_tables.sql
+++ sql/gtfs/conflation_map_bus_aadt/create_conflation_map_bus_aadt_tables.sql
@@ -1,5 +1,6 @@
 \set schema_name 'gtfs'
-\set root_tbl_name :schema_name'.conflation_map_bus_aadt_':YEAR'_v':VERSION
+\set root_tbl_name :schema_name'.conflation_map_bus_aadt_v':VERSION
+\set year_tbl_name :schema_name'.conflation_map_bus_aadt_':YEAR'_':VERSION
 \set tbl_name :schema_name'.conflation_map_bus_aadt_':YEAR'_':AGENCY'_v':VERSION
 \set idx_name 'conflation_map_bus_aadt_':YEAR'_':AGENCY'_v':VERSION'_pkey'
 
@@ -9,22 +10,32 @@ CREATE SCHEMA IF NOT EXISTS :schema_name;
 
 CREATE TABLE IF NOT EXISTS :root_tbl_name (
   conflation_map_id   INTEGER,
-  agency              TEXT,
+  year                INTEGER,
+  transit_agency      TEXT,
   aadt                REAL,
   aadt_by_peak        JSONB,
   aadt_by_route       JSONB,
 
-  PRIMARY KEY (conflation_map_id, agency)
-) PARTITION BY LIST (agency) ;
+  PRIMARY KEY (conflation_map_id, year, transit_agency)
+) PARTITION BY LIST (year) ;
+
+CREATE TABLE IF NOT EXISTS :year_tbl_name
+  PARTITION OF :root_tbl_name
+  FOR VALUES IN (:YEAR)
+  PARTITION BY LIST (transit_agency)
+;
 
 CREATE TABLE IF NOT EXISTS :tbl_name
-  PARTITION OF :root_tbl_name
+  PARTITION OF :year_tbl_name
   FOR VALUES IN (:'AGENCY')
   WITH (fillfactor=100)
 ;
 
 ALTER TABLE :tbl_name
-  ALTER COLUMN agency SET DEFAULT :'AGENCY';
+  ALTER COLUMN transit_agency SET DEFAULT :'AGENCY';
+
+ALTER TABLE :tbl_name
+  ALTER COLUMN year SET DEFAULT :YEAR;
 
 CLUSTER :tbl_name USING :idx_name ;
 
---
+++ sql/gtfs/conflation_map_bus_aadt/create_conflation_map_bus_aadt_views.sql
@@ -0,0 +1,39 @@
+\set schema_name 'gtfs'
+\set root_tbl_name :schema_name'.conflation_map_bus_aadt_v':VERSION
+\set view_name :schema_name'.conflation_map_bus_aadt'
+\set canonical_view_name :schema_name'.bus_aadt'
+
+BEGIN;
+
+-- Should serve only as alias table for
+--   the cannonical year/version table.
+DROP VIEW IF EXISTS :view_name CASCADE;
+
+CREATE VIEW :view_name
+  AS
+    SELECT
+        *
+      FROM :root_tbl_name
+;
+
+CREATE VIEW :canonical_view_name
+  AS
+    SELECT
+        conflation_map_id,
+        year,
+        jsonb_agg(transit_agency ORDER BY transit_agency) AS agencies,
+        SUM(aadt) AS aadt,
+        -- TODO: use jsonb_each_text to aggregate across agencies.
+        json_object_agg(
+          transit_agency,
+          aadt_by_peak
+        ) AS aadt_by_peak,
+        json_object_agg(
+          transit_agency,
+          aadt_by_route
+        ) AS aadt_by_route
+      FROM :view_name
+      GROUP BY conflation_map_id, year
+;
+
+COMMIT;
