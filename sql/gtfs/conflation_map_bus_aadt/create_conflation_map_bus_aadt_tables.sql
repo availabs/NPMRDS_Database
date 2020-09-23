3706be9851aceef46577f5924c713678a9242e8f
Paul Tomchik <pauljtomchik@gmail.com>
2020-09-23 10:50:00 -0400

    Added 'v' before version for gtfs aadt tables.

--- sql/gtfs/conflation_map_bus_aadt/create_conflation_map_bus_aadt_tables.sql
+++ sql/gtfs/conflation_map_bus_aadt/create_conflation_map_bus_aadt_tables.sql
@@ -1,7 +1,7 @@
 \set schema_name 'gtfs'
 \set data_schema_name 'gtfs_data'
 \set root_tbl_name :schema_name'.conflation_map_bus_aadt_v':VERSION
-\set year_tbl_name :schema_name'.conflation_map_bus_aadt_':YEAR'_':VERSION
+\set year_tbl_name :schema_name'.conflation_map_bus_aadt_':YEAR'_v':VERSION
 \set tbl_name :data_schema_name'.conflation_map_bus_aadt_':YEAR'_':AGENCY'_v':VERSION
 \set idx_name 'conflation_map_bus_aadt_':YEAR'_':AGENCY'_v':VERSION'_pkey'
 
