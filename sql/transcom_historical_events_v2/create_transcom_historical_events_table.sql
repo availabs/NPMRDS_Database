25da858d843e5564494b7b47ff928b940e717b9d
Paul Tomchik <pauljtomchik@gmail.com>
2021-09-16 10:00:57 -0400

    transcom_historical_events_v2 schema like transcom_historical_events_v1 schema but with additional cols.

--- sql/transcom_historical_events_v2/create_transcom_historical_events_table.sql
+++ sql/transcom_historical_events_v2/create_transcom_historical_events_table.sql
@@ -3,54 +3,41 @@ BEGIN;
 CREATE SCHEMA IF NOT EXISTS transcom;
 
 CREATE TABLE IF NOT EXISTS transcom.transcom_historical_events_v2 (
-  event_id                    TEXT PRIMARY KEY,
-  event_state                 SMALLINT,
-  event_msg                   TEXT,
+  event_id                TEXT PRIMARY KEY,
+  event_type              TEXT,
 
-  icon_file                   TEXT,
-  event_type                  TEXT,
-  facility                    TEXT,
-  playback_text               TEXT,
-  full_text                   TEXT,
-  sort_order                  SMALLINT,
-  state                       TEXT,
-  county                      TEXT,
-  event_impact                TEXT,
-  relationship                TEXT,
-  mile_marker                 TEXT,
-  events_layer                TEXT,
-  direction                   TEXT,
-  notes                       TEXT,
-  class_name                  TEXT,
-  image_name                  TEXT,
-  show_route_no               SMALLINT,
-  route_no                    TEXT,
-  overlap_events_with_length  TEXT,
-  is_overlapping              SMALLINT,
+  facility                TEXT,
 
-  start_date_time             TIMESTAMP WITHOUT TIME ZONE,
-  end_date                    TIMESTAMP WITHOUT TIME ZONE,
-  last_update_date            TIMESTAMP WITHOUT TIME ZONE,
+  creation                TIMESTAMP WITHOUT TIME ZONE,
+  open_time               TIMESTAMP WITHOUT TIME ZONE,
+  close_time              TIMESTAMP WITHOUT TIME ZONE,
+  duration                TEXT,
 
-  category_name               TEXT,
-  is_latest_event             BOOLEAN,
-  is_highway                  BOOLEAN,
+  state                   TEXT,
+  from_count              TEXT,
+  from_city               TEXT,
+  to_city                 TEXT,
 
-  to_state                    TEXT,
-  to_city                     TEXT,
-  to_facility                 TEXT,
-  to_direction                TEXT,
+  description             TEXT,
 
-  latitude                    DOUBLE PRECISION NOT NULL,
-  longitude                   DOUBLE PRECISION NOT NULL,
-  to_latitude                 DOUBLE PRECISION,
-  to_longitude                DOUBLE PRECISION,
+  from_mile_marker        DOUBLE PRECISION,
+  to_mile_marker          DOUBLE PRECISION,
 
-  event_category              TEXT,
-  point_geom                  public.Geometry(Point, 4326),
+  latitude                DOUBLE PRECISION,
+  longitude               DOUBLE PRECISION,
 
-  _created_timestamp          TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  _modified_timestamp         TIMESTAMP WITHOUT TIME ZONE NOT NULL
+  direction               TEXT,
+
+  -- recovery_time field added in 2018.
+  recovery_time           TEXT,
+  recovery_date_time      TEXT,
+
+  event_category          TEXT,
+
+  point_geom              public.Geometry(Point,4326),
+
+  _created_timestamp      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  _modified_timestamp     TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
 
 CREATE OR REPLACE FUNCTION transcom.transcom_historical_events_v2_trg_created_ts()
@@ -93,11 +80,11 @@ CREATE TRIGGER transcom_historical_events_v2_modified_timestamp
 ;
 
 CREATE INDEX IF NOT EXISTS transcom_historical_events_v2_date_index
-  ON transcom.transcom_historical_events_v2 (start_date_time, end_date)
+  ON transcom.transcom_historical_events_v2 (open_time, close_time)
 ;
 
 CREATE INDEX IF NOT EXISTS transcom_historical_events_v2_year_idx
-  ON transcom.transcom_historical_events_v2 (date_part('year'::text, start_date_time), end_date)
+  ON transcom.transcom_historical_events_v2 (date_part('year'::text, open_time))
 ;
 
 CREATE INDEX IF NOT EXISTS transcom_historical_events_v2_geom_index
