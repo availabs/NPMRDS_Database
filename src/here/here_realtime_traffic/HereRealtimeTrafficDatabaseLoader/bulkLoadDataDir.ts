8117e66fba4738db8be0711880b0377ce9822cb4
Paul Tomchik <pauljtomchik@gmail.com>
2021-11-04 16:19:21 -0400

    Added HereRealtimeTrafficDatabaseLoader bulkLoadDataDir.ts script.

---
+++ src/here/here_realtime_traffic/HereRealtimeTrafficDatabaseLoader/bulkLoadDataDir.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-restricted-syntax */
+
+import { readdirSync } from "fs";
+import { join } from "path";
+
+import { default as HereRealtimeTrafficDatabaseLoaderFactory } from ".";
+
+const pg_env = "development";
+
+const loader =
+  HereRealtimeTrafficDatabaseLoaderFactory.makeHereRealtimeTrafficDatabaseLoader(
+    pg_env
+  );
+
+const hereRealtimeTrafficDataDir = join(__dirname, "../../../../data/here/");
+
+const hereRealtimeTrafficJsonGzips = readdirSync(hereRealtimeTrafficDataDir)
+  .filter((f) => /^here-realtime-traffic\..*\.json\.gz$/.test(f))
+  .sort()
+  .map((f) => join(hereRealtimeTrafficDataDir, f));
+
+async function main() {
+  loader.initializeDatabase();
+
+  await loader.bulkLoadDataFiles(hereRealtimeTrafficJsonGzips);
+}
+
+main();
