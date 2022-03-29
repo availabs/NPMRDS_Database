73507987602bfedda1417351eb9b9065e9548ab0
Paul Tomchik <pauljtomchik@gmail.com>
2022-03-28 23:28:20 -0400

    transcom_event_to_osm

--- sql/transcom_historical_events/create_transcom_historical_events_table.sql
+++ sql/transcom_historical_events/create_transcom_historical_events_table.sql
@@ -78,12 +78,69 @@ CREATE TRIGGER transcom_historical_events_insert_trigger
 CREATE OR REPLACE FUNCTION transcom.transcom_historical_events_update_fn()
   RETURNS TRIGGER AS $$
     BEGIN
-      NEW._modified_timestamp = NOW();
+      IF (
+        ( COALESCE(OLD.event_type, 'OLD')                 <>  COALESCE(NEW.event_type, 'NEW') )
+        OR
+        ( COALESCE(OLD.facility, 'OLD')                   <>  COALESCE(NEW.facility, 'NEW') )
+        OR
+        (
+          COALESCE(OLD.creation, (NOW() - '1 second'::INTERVAL))
+          <>  COALESCE(NEW.creation, (NOW() - '1 second'::INTERVAL))
+        )
+        OR
+        (
+          COALESCE(OLD.open_time, (NOW() - '1 second'::INTERVAL))
+          <>  COALESCE(NEW.open_time, (NOW() - '1 second'::INTERVAL))
+        )
+        OR
+        (
+          COALESCE(OLD.close_time, (NOW() - '1 second'::INTERVAL))
+          <>  COALESCE(NEW.close_time, (NOW() - '1 second'::INTERVAL))
+        )
+        OR
+        ( COALESCE(OLD.duration, 'OLD')                   <>  COALESCE(NEW.duration, 'NEW') )
+        OR
+        ( COALESCE(OLD.description, 'OLD')                <>  COALESCE(NEW.description, 'NEW') )
+        OR
+        ( COALESCE(OLD.from_city, 'OLD')                  <>  COALESCE(NEW.from_city, 'NEW') )
+        OR
+        ( COALESCE(OLD.from_count, 'OLD')                 <>  COALESCE(NEW.from_count, 'NEW') )
+        OR
+        ( COALESCE(OLD.to_city, 'OLD')                    <>  COALESCE(NEW.to_city, 'NEW') )
+        OR
+        ( COALESCE(OLD.state, 'OLD')                      <>  COALESCE(NEW.state, 'NEW') )
+        OR
+        ( COALESCE(OLD.from_mile_marker, -1.0)            <>  COALESCE(NEW.from_mile_marker, 1.0) )
+        OR
+        ( COALESCE(OLD.to_mile_marker, -1.0)              <>  COALESCE(NEW.to_mile_marker, 1.0) )
+        OR
+        ( COALESCE(OLD.latitude, -1.0)                    <>  COALESCE(NEW.latitude, 1.0) )
+        OR
+        ( COALESCE(OLD.longitude, -1.0)                   <>  COALESCE(NEW.longitude, 1.0) )
+        OR
+        ( COALESCE(OLD.event_category, 'OLD')             <>  COALESCE(NEW.event_category, 'NEW') )
+        OR
+        ( COALESCE(OLD.direction, 'OLD')                  <>  COALESCE(NEW.direction, 'NEW') )
+        OR
+        ( COALESCE(OLD.recovery_time, 'OLD')              <>  COALESCE(NEW.recovery_time, 'NEW') )
+        OR
+        ( COALESCE(OLD.recovery_date_time, 'OLD')         <>  COALESCE(NEW.recovery_date_time, 'NEW') )
+        OR
+        ( COALESCE(OLD.duration_interval, '-1 SECOND')    <>  COALESCE(NEW.duration_interval, '1 SECOND') )
+      ) THEN
+        -- Not updated through congestion_data calculation.
+        NEW._modified_timestamp = NOW() ;
+      END IF;
 
       NEW.congestion_data = COALESCE(
         NEW.congestion_data,
         CASE
           WHEN (
+            (
+              COALESCE(OLD.description, '')
+              <> COALESCE(NEW.description, '')
+            )
+            OR
             (
               COALESCE(OLD.creation, '1900-01-01 00:00:00')
               <> COALESCE(NEW.creation, '1900-01-01 00:00:00')
