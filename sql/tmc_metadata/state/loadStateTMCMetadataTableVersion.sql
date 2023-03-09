e819106274e75a17aa40fd9e2c5925f9abce0268
Paul Tomchik <pauljtomchik@gmail.com>
2023-03-09 16:30:52 -0500

    Fix tmc_metadata table creation issue caused by RITIS inconsistency.

--- sql/tmc_metadata/state/loadStateTMCMetadataTableVersion.sql
+++ sql/tmc_metadata/state/loadStateTMCMetadataTableVersion.sql
@@ -50,7 +50,11 @@ CREATE TEMPORARY TABLE tmp_tmc2mpo
                 shp.*
               FROM :shp_tbl AS shp
                 INNER JOIN state_abbreviations AS sabbr
-                  ON (UPPER(shp.state) = UPPER(sabbr.state_name))
+                  ON (
+                    ( UPPER(shp.state) = UPPER(sabbr.state_name) )
+                    OR
+                    ( UPPER(shp.state) = UPPER(sabbr.abbreviation) )
+                  )
               WHERE ( sabbr.abbreviation = :'STATE' )
           ) AS state_shp INNER JOIN mpo_boundaries_view AS mpob
               ON ( mpob.wkb_geometry && state_shp.wkb_geometry )
